8ab5b2814f4e7758cf6d4ff50ef51ebb92de598a
Jinzhe Zeng <njzjz@qq.com>
2018-09-10 14:31:21 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='GaussianRunner',
+      version='1.0.0',
+      description='A script to run Gaussian automatically.',
+      keywords="Gaussian",
+      url='https://github.com/njzjz/GaussianRunner',
+      author='Jinzhe Zeng',
+      author_email='njzjz@qq.com',
+      packages=['GaussianRunner'],
+      install_requires=[])
+
