387715db29ae390ff1104114129daa02f768abb6
Jinzhe Zeng <njzjz@qq.com>
2019-01-15 04:38:29 +0800

    update version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name='gaussianrunner',
-      version='1.0.12',
+      version='1.0.13',
       description='A script to run Gaussian automatically.',
       keywords="Gaussian",
       url='https://github.com/njzjz/gaussianrunner',
